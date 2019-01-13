737e1dc4fe527ab934725adb842468d5b61556c7
archiewald <artur.kozubek1@gmail.com>
2019-01-13 12:54:01 +0100

    replace fetch with blank html

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,22 @@
 /* eslint-disable no-restricted-globals */
-self.addEventListener("fetch", event => {
-  if (
-    event.request.url.endsWith(".jpg") ||
-    event.request.url.endsWith(".jpeg") ||
-    event.request.url.endsWith(".gif") ||
-    event.request.url.endsWith(".png")
-  ) {
-    event.respondWith(
-      fetch(`/image/nointernet.gif`).then(response => response)
-    );
-  }
 
-  caches.open("name-cache").then(cache => {
-    var fileName = "/image/nointernet.gif";
-    fetch(fileName).then(response => {
-      cache.put(fileName, response.clone());
-    });
+self.addEventListener("install", () => {
+  caches.open("app").then(cache => {
+    cache.addAll(["./"]);
   });
+});
+
+self.addEventListener("fetch", event => {
+  //   if (
+  //     event.request.url.endsWith(".jpg") ||
+  //     event.request.url.endsWith(".jpeg") ||
+  //     event.request.url.endsWith(".gif") ||
+  //     event.request.url.endsWith(".png")
+  //   ) {
+  //     event.respondWith(
+  //       fetch(`/image/nointernet.gif`).then(response => response)
+  //     );
+  //   }
+
+  event.respondWith(caches.open("app").then(cache => cache.match("./")));
 });
